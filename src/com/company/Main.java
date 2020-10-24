9f634e5736b211f2d2c5b1fed806cdcd11f994a4
1ntr0men <vadruf07@gmail.com>
2020-10-24 19:07:44 +0300

    Seventh Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble();
-        if (a > 0 && a < 1000 && a % 5 == 0) {
-            System.out.print("true");
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        if (y >= 0 && y <= 0,5 && x >= 0 && y >= Math.sin(Math.toRadians(x))){
+            System.out.print("YES");
         } else {
-            System.out.print("false");
+            System.out.print("NO");
         }
     }
 }
